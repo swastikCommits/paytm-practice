c66ebd6d20192c342d701915f2b777407dfc7492
swastikCommits <textswastik.alt@gmail.com>
2025-06-20 10:53:32 +0530

    init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+// import { Dashboard } from "./pages/Dashboard";
+// import { Send } from "./pages/Send";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      Hello World
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="/send" element={<Send />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
