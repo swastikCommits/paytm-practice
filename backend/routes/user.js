9d2f324681d6625e1a23f880f5a9acc4fa9e1bb6
swastikCommits <textswastik.alt@gmail.com>
2025-06-20 08:13:01 +0530

    init

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config");
 const bcrypt = require("bcrypt");
+
 
 const signUpBody = zod.object({
     username: zod.string().email(),
@@ -12,7 +13,6 @@ const signUpBody = zod.object({
     lastName: zod.string().min(1),
     password: zod.string().min(1)
 })
-
 
 router.post("/signup", async (req, res) => {
 
@@ -41,12 +41,58 @@ router.post("/signup", async (req, res) => {
 
    const userId = user._id;
 
-   const token = jwt.sign({userId}, JWT_SECRET);
+   const token = jwt.sign({userId}, process.env.JWT_SECRET);
 
    res.status(200).json({
     message: "User created successfully",
     token: token
    })
 })
+
+
+
+
+const signInBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
+
+router.post("/signin", async (req, res) => {
+
+    const { success } = signInBody.safeParse(req.body);
+    if(!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username, 
+        password: req.body.password
+    });
+    if(!user) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    if(!isPasswordCorrect) {        
+        return res.status(411).json({
+            message: "Incorrect password"
+        })
+    }
+
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
+
+    res.status(200).json({
+        message: "User signed in successfully",
+        token: token
+    })
+    
+
+})
+
+
 
 module.exports = router;
