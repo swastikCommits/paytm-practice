0aa8fc3b461a959b2e91a100ee898f081b4ab57c
swastikCommits <textswastik.alt@gmail.com>
2025-06-21 09:35:43 +0530

    init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
-// import { Dashboard } from "./pages/Dashboard";
-// import { Send } from "./pages/Send";
+import { Dashboard } from "./pages/Dashboard";
+import { Send } from "./pages/Send";
 
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          {/* <Route path="/send" element={<Send />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<Send />} />
         </Routes>
       </BrowserRouter>
     </>
